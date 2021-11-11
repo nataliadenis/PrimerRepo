b79368395e37d5b0adb107434cc52dba552c793f
Natalia Denis <natalia.denis@mercadolibre.com>
2021-11-11 17:19:49 -0300

    ejercicio repaso js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("hola mundo")
+function calculatePrice(name, price){
+    let ship;
+    price > 0 && price < 2000? ship = 300: price <4000? ship=500 : ship = 700;
+    console.log(`El producto ${name} cuesta ${price} . 
+        Su costo de envío es de ${ship}. Por lo tanto, el precio final es de ${price+ship}`)
+}
 
+calculatePrice("mackbook",2500);
+
+calculatePrice('Celular', 500);
+
+calculatePrice('Playstation', 4500);
